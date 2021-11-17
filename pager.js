9500c08078af3c110094ea073c27cc97a1cd9c5b
Maria Lapshina <lapmarser@gmail.com>
2021-11-16 20:46:46 -0500

    another set of changhes

--- pager.js
+++ pager.js
@@ -1,1 +1,11 @@
 // 
+const employees = document.querySelector('.employees')
+
+fetch('https://acme-users-api-rev.herokuapp.com/api/users')
+  .then(response => response.json())
+  .then(data => {
+      const html = data.users.map(user => `<li><div>${user.firstName}</div><div>${user.lastName}</div><div>${user.email}</div><div>${user.title}</div></li>`)
+      //console.log(html.join(''))
+      employees.innerHTML = html.join('')
+  })
+  console.log(employees)
